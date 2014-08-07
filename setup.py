e3f2a37771d5f3b1cef18de28501697d2dbdbf8d
Victor Fernandez de Alba <sneridagh@gmail.com>
2014-08-07 14:25:38 +0200

    Back to development: 2.0b2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '2.0b1'
+version = '2.0b2.dev0'
 
 README = open("README.rst").read()
 HISTORY = open(os.path.join("docs", "HISTORY.rst")).read()
